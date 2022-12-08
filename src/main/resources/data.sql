8721664e7fe357893ab59fd6f60302ee39648b70
Aram Virabyan <googlaram200@gmail.com>
2022-12-08 20:26:59 +0300

    upd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,49 @@
 DELETE
+FROM recycle_points;
+
+
+DELETE
+FROM users;
+
+
+INSERT INTO users (name, password)
+VALUES ('Anatoly',
+        '123');
+
+
+INSERT INTO users (name, password)
+VALUES ('Aram',
+        '456');
+
+
+
+
+/*
+INSERT INTO recycle_points (user_id, title, description, date_of_creation, date_of_completion)
+VALUES (1,
+        'Справка',
+        'Взять справку в поликлинике',
+        '2022-12-06',
+        NULL);
+
+
+INSERT INTO recycle_points (user_id, title, description, date_of_creation, date_of_completion)
+VALUES (1,
+        'Хакатон',
+        'Победить',
+        '2022-12-11',
+        NULL);
+
+
+INSERT INTO recycle_points (user_id, title, description, date_of_creation, date_of_completion)
+VALUES (2,
+        'Хакатон',
+        'Победить',
+        '2022-12-11',
+        NULL);
+/*
+/*
+DELETE
 FROM tasks;
 
 
@@ -38,3 +83,4 @@ VALUES (2,
         'Победить',
         '2022-12-11',
         NULL);
+/*
