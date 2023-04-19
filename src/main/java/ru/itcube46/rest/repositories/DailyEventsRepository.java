92af9dcdf8cd438116ce293e4af6609a02439799
RomanK11 <romankorovin032@gmail.com>
2023-04-19 10:03:19 +0300

    .

--- src/main/java/ru/itcube46/rest/repositories/DailyEventsRepository.java
+++ src/main/java/ru/itcube46/rest/repositories/DailyEventsRepository.java
@@ -7,12 +7,12 @@ import org.springframework.data.repository.query.Param;
 import ru.itcube46.rest.entities.DailyEvents;
 
 public interface DailyEventsRepository extends CrudRepository<DailyEvents, Long> {
-    @Query("SELECT * FROM DAILY_EVENTS WHERE EVENT_ID = :eid AND EVENT_NAME = :eName")
-    Iterable<DailyEvents> findEventByIdAndName(@Param("eid") Long eventId, @Param("eName") String eventName);
+    @Query("SELECT * FROM DAILY_EVENTS WHERE QUIZ_ID = :qid AND EVENT_NAME = :eName")
+    Iterable<DailyEvents> findEventByIdAndName(@Param("qid") Long eventId, @Param("eName") String eventName);
 
     
     @Query("SELECT * FROM DAILY_EVENTS WHERE EVENT_ID = :eid")
-    Iterable<DailyEvents> findEventById(@Param("eid") Long eventId);
+    Iterable<DailyEvents> findEventById(@Param("qid") Long eventId);
 
 
     
