345340717a024a7eeedf374c8f64db42c7b9a712
RomanK11 <romankorovin032@gmail.com>
2023-04-19 15:48:38 +0300

    m

--- src/main/java/ru/itcube46/rest/repositories/QuestionsRepository.java
+++ src/main/java/ru/itcube46/rest/repositories/QuestionsRepository.java
@@ -1,20 +1,17 @@
 package ru.itcube46.rest.repositories;
-
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import ru.itcube46.rest.entities.Quizzes;
+import ru.itcube46.rest.entities.Questions;
+public interface QuestionsRepository extends CrudRepository<Questions, Long> { 
 
-public interface QuizzesRepository extends CrudRepository<Quizzes, Long> {
-    /*@Query("SELECT * FROM QUESTIONS WHERE QUESTION_ID = :qid AND DIFFICULTY = :difficulty")
-    Iterable<Questions> findAllByQuestionIdAndDifficulty(@Param("qid") Long questionId, @Param("difficulty") String difficulty);
-*/
-    @Query("SELECT * FROM QUIZZES WHERE DIFFICULTY = :difficulty")
-    Iterable<Quizzes> findAllByDifficulty(@Param("difficulty") String difficulty);
+   @Query("SELECT * FROM QUESTIONS WHERE QUESTION_SCORES = :questionScores")
+    Iterable<Questions> doubleSumQuestionsScores(@Param("questionScores") Long questionScores);
 
-    @Query("SELECT * FROM QUIZZES WHERE DIFFICULTY = :difficulty ORDER BY RAND() LIMIT 1")
-    Iterable<Quizzes> findOneByDifficulty(@Param("difficulty") String difficulty);
-    
-    
+    @Query("SELECT * FROM QUESTIONS WHERE QUESTION_SCORES = :questionScores")
+    Iterable<Questions> findScoresByQuizId(@Param("questionScores") Long questionScores);
+
+    @Query("SELECT * FROM QUESTIONS WHERE quiz_id = :quizId")
+    Iterable<Questions> findQuestionsByQuizId(@Param("quizId") Long quizId);
 }
